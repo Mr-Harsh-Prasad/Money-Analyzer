7f4fdb6c2b13acde8505e834e2d85e2364196686
Mr-Harsh-Prasad <mrharshdrive@gmail.com>
2026-01-21 14:32:58 +0530

    Fix login redirect URL

--- config/settings.py
+++ config/settings.py
@@ -89,3 +89,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
